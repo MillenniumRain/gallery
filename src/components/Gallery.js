276181152fac19e299c43fe38e7e92d6b2088f96
Nikita Davydov <nik210597@gmail.com>
2021-05-23 16:22:56 +0500

    Конец

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -6,14 +6,34 @@ export class Gallery {
 		this.div.addEventListener('click', (e) => {
 			if (e.target.classList.contains('gallery__remove')) this.remove(e);
 		});
+		window.onresize = () => {
+			let img = document.querySelectorAll('.gallery__img');
+			for (let i = 0; i < img.length; i++) {
+				img[i].style = '';
+			}
+			this.resizeImages(img);
+		};
 		main.append(this.div);
 	}
+	// timeToResize(params) {
+	// 	setTimeout(() => {
+	// 		let img = document.querySelectorAll('.gallery__img');
+	// 		for (let i = 0; i < img.length; i++) {
+	// 			img[i].style = '';
+	// 		}
+	// 		this.resizeImages(img);
+	// 		this.timeToResize();
+	// 	}, 250);
+	// }
 	remove(e) {
 		e.target.closest('.gallery__container').outerHTML = '';
+		let images = document.querySelectorAll('.gallery__img');
+		this.resizeImages(images);
 	}
 	get() {
 		return this.div;
 	}
+
 	draw(data) {
 		data = JSON.parse(data);
 		let html = ``;
@@ -21,9 +41,61 @@ export class Gallery {
 			html += this.cardTamplate(value.url, index);
 		});
 		this.div.innerHTML += html;
+		this.resizeImagesOnLoad();
+		// this.timeToResize();
+	}
+	resizeImagesOnLoad() {
+		let images = document.querySelectorAll('.gallery__img');
+		let counter = 0;
+
+		images.forEach((val) => {
+			val.onload = () => {
+				counter++;
+				if (counter == images.length) {
+					this.resizeImages(images);
+				}
+			};
+		});
+	}
+	resizeImages(images) {
+		let countInRow = 1;
+		let widthtInRow = images[0].offsetWidth;
+		for (let i = 0; i < images.length; i++) {
+			if (widthtInRow > gallery.offsetWidth) {
+				this.resizeImagesInRow(
+					images,
+					i,
+					this.div,
+					countInRow,
+					widthtInRow + 2
+				);
+				countInRow = 1;
+				widthtInRow = images[i + 1]
+					? images[i + 1].offsetWidth
+					: images[i].offsetWidth;
+				// break;
+			} else {
+				countInRow++;
+				widthtInRow += images[i + 1]
+					? images[i + 1].offsetWidth
+					: images[i].offsetWidth;
+			}
+		}
+	}
+	resizeImagesInRow(images, index, gallery, countInRow, widthtInRow) {
+		let blockWidth = gallery.offsetWidth;
+		let rowWidth = widthtInRow;
+
+		let ratio = rowWidth / images[index].offsetHeight;
+		let height = (rowWidth - blockWidth) / ratio;
+
+		for (let i = 0; i < countInRow; i++) {
+			let h = images[index + 1 - countInRow + i].offsetHeight - height;
+			images[index + 1 - countInRow + i].style.height = h + 'px';
+		}
 	}
 	cardTamplate(src, id) {
-		return `<div class="gallery__container"">
+		return `<div class="gallery__container">
 			<img  class="gallery__img" src="${src}" alt="" />
 			<div class="gallery__remove">&#10006;</div>
 		</div>`;
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
 	context: path.resolve(__dirname, 'src'),
 	entry: {
